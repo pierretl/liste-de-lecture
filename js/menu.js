5f3355ba5a3a1dc3401fc44f0f8cb0dfcb671034
PierreTL <pierre.tampon@gmail.com>
2022-01-17 21:55:27 +0100

    Correctif overlay on touch screen

--- js/menu.js
+++ js/menu.js
@@ -36,9 +36,15 @@ for (let i = 0; i < btnMenuDisplay.length; i++) {
     btnMenuDisplay[i].addEventListener('click', displayMenu, false);
 }
 
-// Ajoute un overlay au site au surlvol des bouton du menu
+// Ajoute un overlay au site au survol des boutons du menu
 for (let i = 0; i < btnMenu.length; i++) {
-    btnMenu[i].addEventListener('mouseover', overlayContentAdd, false);
-    btnMenu[i].addEventListener('mouseout', overlayContentRemove, false);
+    if (Modernizr.touch) { 
+        btnMenu[i].addEventListener('touchstart', overlayContentAdd, false);
+        btnMenu[i].addEventListener('touchend', overlayContentRemove, false);
+      } else { 
+        btnMenu[i].addEventListener('mouseover', overlayContentAdd, false);
+        btnMenu[i].addEventListener('mouseout', overlayContentRemove, false);
+      }
+    
 }
 
