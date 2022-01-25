64a3eb45cbc020ddcb46ca64185cbae934a247da
PierreTL <pierre.tampon@gmail.com>
2022-01-26 00:01:56 +0100

    Changement de la position du menu aux cliques sans passer par l'écran de sélection

--- js/2-emplacement-menu.js
+++ js/2-emplacement-menu.js
@@ -3,35 +3,50 @@ const currentPosition = localStorage.getItem('positionMenu');
 const positionMenuBtn = document.querySelector('.js_positionMenu-btn');
 const positionMenuBtnGauche = document.querySelector('.js_positionMenu-gauche');
 const positionMenuBtnDroite = document.querySelector('.js_positionMenu-droite');
+const texteGauche = document.querySelector('.js_texte-gauche');
+const texteDroite = document.querySelector('.js_texte-droite');
 
-function choixPositionMenu(menuParDefaut) {
-    positionMenuBtn.classList.add(':click');
-    menu.classList.remove(':visible');
-    positionMenuBtnGauche.classList.remove('hide');
-    positionMenuBtnDroite.classList.remove('hide');
-    app.classList.add(classOverlay);
+function choixPositionMenu() {
+    if (localStorage.getItem('positionMenu') == 'droite') {
+        localStorage.setItem('positionMenu', 'gauche');
+        texteGauche.classList.add('hide');
+        texteDroite.classList.remove('hide');
+    } else {
+        localStorage.setItem('positionMenu', 'droite');
+        texteDroite.classList.add('hide');
+        texteGauche.classList.remove('hide');
+    }
+    document.documentElement.dataset.position = localStorage.getItem('positionMenu');
 }
 
 function changePosition() {
-    positionMenuBtn.classList.remove(':click');
-    menu.classList.add(':visible');
     positionMenuBtnGauche.classList.add('hide');
     positionMenuBtnDroite.classList.add('hide');
     app.classList.remove(classOverlay);
     if (this.classList.contains('js_positionMenu-gauche')){
         localStorage.setItem('positionMenu', 'gauche');
+        texteGauche.classList.add('hide');
+        texteDroite.classList.remove('hide');
     } else {
         localStorage.setItem('positionMenu', 'droite');
+        texteDroite.classList.add('hide');
+        texteGauche.classList.remove('hide');
     }
     document.documentElement.dataset.position = localStorage.getItem('positionMenu');
 }
 
 if ( localStorage.getItem('positionMenu') ) {
     document.documentElement.dataset.position = localStorage.getItem('positionMenu');
+    if (localStorage.getItem('positionMenu') == 'droite') {
+        texteDroite.classList.add('hide');
+    } else {
+        texteGauche.classList.add('hide');
+    }
+    console.log("local");
 } else {
-    localStorage.setItem('positionMenu', 'gauche');
-    choixPositionMenu();
-    menu.classList.remove(':visible');
+    positionMenuBtnGauche.classList.remove('hide');
+    positionMenuBtnDroite.classList.remove('hide');
+    app.classList.add(classOverlay);
 }
 
 positionMenuBtn.addEventListener('click', choixPositionMenu, false);
