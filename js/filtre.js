c51b6cd7426ff732e5149c1e5d3154af2c994693
PierreTL <pierre.tampon@gmail.com>
2022-01-29 12:04:31 +0100

    Ajout d'un filtre rapide : liste d'achats

--- js/filtre.js
+++ js/filtre.js
@@ -1,5 +1,6 @@
 const filtre = document.querySelector(".js_filtre");
 const filtreBtn = document.querySelector(".js_filtre-btnDisplay");
+const filtreAchat = document.querySelector(".js_fitre-achat");
 
 filtreBtn.addEventListener('click', function () {
     if (filtre.classList.contains('hide')){
@@ -8,4 +9,10 @@ filtreBtn.addEventListener('click', function () {
     } else {
         filtre.classList.add('hide');
     }
-})
+});
+
+filtreAchat.addEventListener('click', function () {
+    document.getElementById('encours').checked = true;
+    document.getElementById('possede').checked = true;
+    document.getElementById('btnPreference').click();
+});
